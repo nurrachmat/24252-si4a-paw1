64addd0c8d2f9ba7ea43d04735ffd47d54a5b3ec
nurrachmat <rachmat.nur@gmail.com>
2025-04-09 14:56:14 +0700

    Update buku tamu

--- localStorage/script.js
+++ localStorage/script.js
@@ -7,7 +7,8 @@ tampil();
 function simpan(){
     let nama = document.getElementById('nama').value; // ambil value dari input
     let keperluan = document.getElementById('keperluan').value; // ambil value dari input
-    console.log(nama);
+    let jk = document.getElementById('jk').value;
+    console.log(jk);
 
     // cek apakah local storage kosong
     if(localStorage.getItem('ls_bukutamu') == null){
@@ -17,7 +18,7 @@ function simpan(){
         data = JSON.parse(localStorage.getItem('ls_bukutamu'));
     }
 
-    data.push({nama_pengunjung: nama, perlu: keperluan}); // masukkan value input nama ke dalam array
+    data.push({nama_pengunjung: nama, perlu: keperluan, jk: jk}); // masukkan value input nama ke dalam array
     
     localStorage.setItem('ls_bukutamu', JSON.stringify(data)); // simpan ke local storage
 
@@ -30,9 +31,25 @@ function simpan(){
 function tampil(){
     localStorage.getItem('ls_bukutamu') == null ? data = [] : data = JSON.parse(localStorage.getItem('ls_bukutamu')); // cek apakah local storage kosong
 
+    console.log(data.length); // tampilkan jumlah data di console
+
+    document.getElementById('total_tamu').innerHTML = `<h2>Total tamu <br> ${data.length}</h2>`;
+
+    let total_laki=0; 
+    let total_perempuan=0;
+
     data.forEach( (item) => {
-        daftar_tamu.innerHTML += `<li>
-            ${item.nama_pengunjung} - ${item.perlu}
-        </li>`
+        if(item.jk == 'L'){
+            total_laki++;
+        } else if(item.jk == 'P'){
+            total_perempuan++;
+        }
+
+        daftar_tamu.innerHTML += `<div class='col-lg-2 rounded text-center text-white ${item.jk == 'L' ? 'bg-success' : 'bg-warning' } p-3 m-1'>
+            <h4>${item.nama_pengunjung}</h4>
+            ${item.perlu} <br> ${item.jk}
+        </div>`
     })
+    document.getElementById('tamu_laki').innerHTML = `<h2>Tamu laki-laki <br> ${total_laki}</h2>`;
+    document.getElementById('tamu_perempuan').innerHTML = `<h2>Tamu perempuan <br> ${total_perempuan}</h2>`;
 }
